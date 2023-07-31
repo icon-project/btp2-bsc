de9f27c0b983aeef1dfb50f7ebed30900dbb6568
ByoungGul Kwon <kwon528@iconloop.com>
2023-07-31 17:22:31 +0900

    Refactor factory

--- chain/bsc2/bsc2factory.go
+++ chain/bsc2/bsc2factory.go
@@ -14,15 +14,14 @@ import (
 )
 
 const (
-	TYPE  = "bsc-plato-solidity"
+	TYPE  = "bsc-hertz"
 	Epoch = 200
 )
 
-func RegisterBscPlato() {
+func RegisterBscHertz() {
 	link.RegisterFactory(&link.Factory{
 		Type:             TYPE,
 		ParseChainConfig: ParseChainConfig,
-		NewLink:          NewLink,
 		NewReceiver:      NewReceiver,
 		NewSender:        NewSender,
 	})
@@ -30,32 +29,13 @@ func RegisterBscPlato() {
 
 func ParseChainConfig(raw json.RawMessage) (link.ChainConfig, error) {
 	cfg := chain.BaseConfig{}
-
-	jsonbody, err := json.Marshal(raw)
-	if err != nil {
+	if err := json.Unmarshal(raw, &cfg); err != nil {
 		return nil, err
 	}
-
-	if err := json.Unmarshal(jsonbody, &cfg); err != nil {
-		return nil, err
+	if cfg.Type != TYPE {
+		return nil, fmt.Errorf("invalid type (type:%s)", cfg.Type)
 	}
-
-	//TODO add check
-	if cfg.Type == TYPE {
-		return cfg, nil
-	}
-
-	return nil, nil
-}
-
-func NewLink(srcCfg link.ChainConfig, dstAddr types.BtpAddress, baseDir string, l log.Logger) (types.Link, error) {
-	r, err := NewReceiver(srcCfg, dstAddr, baseDir, l)
-	if err != nil {
-		return nil, err
-	}
-
-	link := link.NewLink(srcCfg, r, l)
-	return link, nil
+	return cfg, nil
 }
 
 func NewReceiver(srcCfg link.ChainConfig, dstAddr types.BtpAddress, baseDir string, l log.Logger) (link.Receiver, error) {
@@ -76,7 +56,7 @@ func NewReceiver(srcCfg link.ChainConfig, dstAddr types.BtpAddress, baseDir stri
 func NewSender(srcAddr types.BtpAddress, dstCfg link.ChainConfig, l log.Logger) (types.Sender, error) {
 	dst := dstCfg.(chain.BaseConfig)
 
-	w, err := newWallet(dst.KeyStorePass, dst.KeySecret, dst.KeyStoreData)
+	w, err := newWallet(dst.KeyStorePass, dst.KeySecret, dst.KeyStore)
 	if err != nil {
 		return nil, err
 	}
@@ -89,12 +69,16 @@ func NewSender(srcAddr types.BtpAddress, dstCfg link.ChainConfig, l log.Logger) 
 		Epoch:      uint64(Epoch)}, w, l), nil
 }
 
-func newWallet(passwd, secret string, keyStore json.RawMessage) (types.Wallet, error) {
-	pw, err := resolvePassword(secret, passwd)
-	if err != nil {
-		return nil, err
+func newWallet(passwd, secret string, keyStorePath string) (types.Wallet, error) {
+	if keyStore, err := os.ReadFile(keyStorePath); err != nil {
+		return nil, fmt.Errorf("fail to open KeyStore file path=%s", keyStorePath)
+	} else {
+		pw, err := resolvePassword(secret, passwd)
+		if err != nil {
+			return nil, err
+		}
+		return wallet.DecryptKeyStore(keyStore, pw)
 	}
-	return wallet.DecryptKeyStore(keyStore, pw)
 }
 
 func resolvePassword(keySecret, keyStorePass string) ([]byte, error) {
