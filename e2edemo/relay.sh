0021cf2f4228d5c27ba1be3028b721eb4a7b5627
ByoungGul Kwon <kwon528@iconloop.com>
2023-08-10 12:31:52 +0900

    Update relay execute shell

--- e2edemo/relay.sh
+++ e2edemo/relay.sh
@@ -4,6 +4,11 @@ RELAY_BIN=../bin/relay
 DEPLOYMENTS=deployments.json
 CHAIN_CONFIG=chain_config.json
 
+
+DB_TYPE=leveldb
+DB_PATH=./data
+START_NUMBER=400
+
 if [ ! -f ${RELAY_BIN} ]; then
     (cd ..; make relay)
 fi
@@ -28,13 +33,63 @@ DST_ENDPOINT=$(cat ${CHAIN_CONFIG} | jq -r .chains.${DST}.endpoint)
 DST_KEY_STORE=$(cat ${CHAIN_CONFIG} | jq -r .chains.${DST}.keystore)
 DST_KEY_PASSWORD=$(cat ${CHAIN_CONFIG} | jq -r .chains.${DST}.keypass)
 
+SRC_NETWORK_NAME=$(echo ${SRC_NETWORK} | cut -d. -f2)
+DST_NETWORK_NAME=$(echo ${DST_NETWORK} | cut -d. -f2)
+
+# Determine src type
+if [ $SRC_NETWORK_NAME == icon ]; then
+  SRC_TYPE="icon-btpblock"
+else
+  SRC_TYPE="bsc-hertz"
+fi
+
+# Determine src type
+if [ "x$BMV_BRIDGE" = xtrue ]; then
+  echo "Using Bridge mode"
+  DST_TYPE="icon-bridge"
+else
+  echo "Using BTPBlock mode"
+  DST_TYPE="icon-btpblock"
+fi
+
+get_bsc_option() {
+    echo '{
+      "db_type": "'$1'",
+      "db_path": "'$2'",
+      "start_number": "'$3'"
+    }' | tr -d [:space:]
+}
+
+BSC_OPTION=$(get_bsc_option "$DB_TYPE" "$DB_PATH" "$START_NUMBER")
+
+get_config() {
+  if [ $# -gt 5 -a ${#6} -gt 0 ]; then
+    echo '{
+      "address": "'$1'",
+      "endpoint": "'$2'",
+      "key_store": "'$3'",
+      "key_password": "'$4'",
+      "type": "'$5'",
+      "options": '$6'
+    }' | tr -d [:space:]
+  else
+    echo '{
+      "address": "'$1'",
+      "endpoint": "'$2'",
+      "key_store": "'$3'",
+      "key_password": "'$4'",
+      "type": "'$5'"
+    }' | tr -d [:space:]
+  fi
+}
+SRC_CONFIG=$(get_config "$SRC_ADDRESS" "$SRC_ENDPOINT" "$SRC_KEY_STORE" "$SRC_KEY_PASSWORD" "$SRC_TYPE" "$BSC_OPTION")
+DST_CONFIG=$(get_config "$DST_ADDRESS" "$DST_ENDPOINT" "$DST_KEY_STORE" "$DST_KEY_PASSWORD" "$DST_TYPE")
+
+echo ${SRC_CONFIG}
+echo ${DST_CONFIG}
 ${RELAY_BIN} \
-    start \
+    --base_dir .relay \
     --direction both \
-    --src.address ${SRC_ADDRESS} \
-    --src.endpoint ${SRC_ENDPOINT} \
-    --src.key_store ${SRC_KEY_STORE} \
-    --dst.address ${DST_ADDRESS} \
-    --dst.endpoint ${DST_ENDPOINT} \
-    --dst.key_store ${DST_KEY_STORE} \
-    --dst.key_password ${DST_KEY_PASSWORD}
+    --src_config ${SRC_CONFIG} \
+    --dst_config ${DST_CONFIG} \
+    start
