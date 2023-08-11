0f57354a04ea253eee0e533d6f7b7d3acb87975d
EunKi Yu <yueunki@parametacorp.com>
2023-08-11 13:45:34 +0900

    Set default gas price for bsc private network

--- e2edemo/hardhat.config.ts
+++ e2edemo/hardhat.config.ts
@@ -15,7 +15,8 @@ const config: HardhatUserConfig = {
         // e2e/docker/bsc2
         // address: 0x04d63aBCd2b9b1baa327f2Dda0f873F197ccd186
         "0x59ba8068eb256d520179e903f43dacf6d8d57d72bd306e1bd603fdb8c8da10e8"
-      ]
+      ],
+      gasPrice: 20000000000,
     },
   },
   solidity: {
